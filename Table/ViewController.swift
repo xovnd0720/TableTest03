334f5e9a331efeb94a7b7ab46feae62eb97fc819
dit000 <dit000@dit000.local>
2019-04-30 14:17:54 +0900

    Initial Commit

---
+++ Table/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table
+//
+//  Created by dit000 on 2019. 4. 30..
+//  Copyright © 2019년 DIT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
